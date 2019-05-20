374f23924481627aba50dca0a476a25d68d8f8ca
abel47 <aioaneiabel@gmail.com>
2019-05-20 17:27:06 +0200

    test

--- library/MyBinaryRelevanceFeatureSelect.py
+++ library/MyBinaryRelevanceFeatureSelect.py
@@ -1,20 +1,23 @@
 from skmultilearn.problem_transform import BinaryRelevance
 from sklearn.svm import SVC
+#from sklearn.base import ProblemTransformationBase
+
 
 class MyBinaryRelevanceFeatureSelect():
    
     def fit(self, X, y):
         
         # I'm using a gaussian naive bayes base classifier
-        self.BinaryRelevanceObject = BinaryRelevance(classifier = SVC(gamma= 'auto'), require_dense = [True, True])
-        
+        self.BinaryRelevanceObject = BinaryRelevance(classifier = SVC(gamma= 'auto', probability=True), require_dense = [True, True])
+        #self.BinaryRelevanceObject = BinaryRelevance()
+
         # fitting the data
         self.BinaryRelevanceObject.fit(X, y)
         
         #the classifiers for each label
         self.classifiers = self.BinaryRelevanceObject.classifiers_
 
-        return self
+        return self.BinaryRelevanceObject.fit(X, y)
         
 #     def partition(self):
 #         return self.BinaryRelevanceObject.partition_#BinaryRelevanceObject
@@ -24,39 +27,21 @@ class MyBinaryRelevanceFeatureSelect():
 
     def predict(self, X, y=None):
         return self.BinaryRelevanceObject.predict(X)
-    
-#     def feature_select(self, X, y):
-        
-#         #features_selected = []
 
-# #         X_new = SelectKBest(chi2, k=2)
-
-# #         # the feature selecting
-# #         X_new.fit(X, y)
-
-# #         # save indices of the saved attributes
-# #         selected_attributes_indices = X_new.get_support(indices = True)
-        
-#         transformer = GenericUnivariateSelect(chi2, 'k_best', param=2)
-#         X_new = transformer.fit_transform(X, y)
-#         #X_new.shape
-#         selected_attributes_indices = transformer.get_support(indices = True)
-        
-#         return selected_attributes_indices
+    def predict_proba(self, X):
+        return self.BinaryRelevanceObject.predict_proba(X)    
     
-    def feature_select(self, X, y, transformer):
-        
-        #transformer = SelectKBest(chi2, k=2)
-        transformer.fit(X, y)
-        selected_attributes_indices = transformer.get_support(indices = True)
-        
-        return selected_attributes_indices
-    
-    def sets_of_selected_features(self, X, predictions, classifier, transformer ): #X is the df with the predictions
-        selected_features_array = []
-
-        for i in predictions:
-            indices_features_selected = classifier.feature_select(X, predictions[i], transformer)
-            selected_features_array.append(indices_features_selected)
-    
-        return selected_features_array
+#    def feature_select(self, X, y, transformer):   
+#        transformer.fit(X, y)
+#        selected_attributes_indices = transformer.get_support(indices = True)
+#        
+#        return selected_attributes_indices
+#    
+#    def sets_of_selected_features(self, X, predictions, classifier, transformer ): #X is the df with the predictions
+#        selected_features_array = []
+#
+#        for i in predictions:
+#            indices_features_selected = classifier.feature_select(X, predictions[i], transformer)
+#            selected_features_array.append(indices_features_selected)
+#    
+#        return selected_features_array
--- library/__init__.py
+++ library/__init__.py
@@ -1,1 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 14 23:09:56 2019
+
+@author: Abel
+"""
 
