a138c664c130f19736c6201a5119d521c25adfcc
abel47 <aioaneiabel@gmail.com>
2019-05-24 23:45:27 +0200

    constructed parts of the two methods cc and ls

--- library/MyLCSFFeatureSelect.py
+++ library/MyLCSFFeatureSelect.py
@@ -13,83 +13,103 @@ import random
 #from scipy import sparse
 
 class lcsf_feature_selection():
-	def __init__():
-#		if q is None: #have to fix things in here
-#			q = 2
-		self.q = q
-		self.selecting_strategy = selecting_strategy
-		self.combining_strategy = combining_strategy
-	def fit(self, X, y):
-		
-		#self.lcsf_object =  
-		
-#		X_new = self.selecting_strategy.fit_transform(X, y)
-#		X_new.shape
-		
-		
-		return self
-	
-	def RS(self, q, y):
-		#rand select two diff numbers in range 0 - length of y
-		#return the labels
-		labels_selected = random.sample(range(0, len(y)), 2)
-		
-		y_prim = []
-		y_prim.append(labels_selected[0])
-		y_prim.append(labels_selected[1])		
-		
-		return y_prim
-	
-	def CS(self, q, y):
-		dictionary = dict() #or tuple or matrix
-		k = 0
-		for i in range(0,len(y)):
-			for j in range(0,len(y)):
-				if i!=j and y[i]==y[j] and y[j] == 1:
-					dictionary[k]
-					pass
-					
-		
-		#nb of labels within a pair (1,1)
-		#select the first q
-		#return the labels
-		pass
-	
-	def LS(self, q, y):
-		# nr ex agree (1,1) AND (0,0)
-		# c_e]
-		Y = y
-		mydict = dict()
+    def __init__():
+#        if q is None: #have to fix things in here
+#            q = 2
+        self.q = q
+        self.selecting_strategy = selecting_strategy
+        self.combining_strategy = combining_strategy
+    def fit(self, X, y):
+        
+        #self.lcsf_object =  
+        
+#        X_new = self.selecting_strategy.fit_transform(X, y)
+#        X_new.shape
+        
+        
+        return self
+    
+    def RS(self, q, y):
+        #rand select two diff numbers in range 0 - length of y
+        #return the labels
+        labels_selected = random.sample(range(0, len(y)), 2)
+        
+        y_prim = []
+        y_prim.append(labels_selected[0])
+        y_prim.append(labels_selected[1])        
+        
+        return y_prim
+    
+    def CS(self, q, y):
+#        dictionary = dict() #or tuple or matrix
+#        k = 0
+#        for i in range(0,len(y)):
+#            for j in range(0,len(y)):
+#                if i!=j and y[i]==y[j] and y[j] == 1:
+#                    dictionary[k]
+#                    pass
+        c_c = [] 
 
-		for y in Y:
-		    c_e = 0
-		    c_d = 0
-		    for i in range(1,len(Y)+1):
-		        #print(len(y))
-		        mydict.update({(i,):None})
-		#         c_e = 0
-		#         c_d = 0
-		        for j in range(i,len(Y)+1):
-		            #mydict.update({(i,j):0})
-		            if i!=j and y[i]==y[j]:
-		                c_e = c_e + 1
-		                mydict.update({(i,j):(c_e,c_d)})
-		            if i!=j and y[i]!=y[j]:
-		                c_d = c_d + 1
-		                mydict.update({(i,j):(c_e, c_d)})
-		
-		#twolists sorted according to 1.c_e 2.c_d
-		
-		pass
-	
-	def generation(self):
-		#AND  y[i][j] = 1 iff y[i]=y[j]=1 else y[i][j]=0
-		#XOR  y[i][j] = 1 iff y[i]!=y[j]  else y[i][j]=0
-		#XNOR y[i][j] = 1 iff y[i]=y[j]   else y[i][j]=0
-		pass
+        cc = 0
+        
+        for i in range(0, y.shape[0]):
+            #cc=0
+            for j in range(i+1, y.shape[0]):
+                if j!=i:
+                    cc=0
+                    for k in range(0, y.shape[1]):
+                        if y[i][k]==y[j][k] and y[i][k] == 1:
+                            cc = cc + 1
+                    c_c.append(cc)    
+                    print(cc)
+        
+        #nb of labels within a pair (1,1)
+        #select the first q
+        #return the labels
+        pass
+    
+    def LS(self, q, y):
+        # nr ex agree (1,1) AND (0,0)
+        # c_e]
+        indices = []
+        
+        c_e = []
+        c_d = []
+        
+        ce = 0
+        cd = 0
+        
+        for i in range(0, y.shape[0]):
+            #cc=0
+            for j in range(i+1, y.shape[0]):
+                if j!=i:
+                    ce = 0
+                    cd = 0
+                    indices.append([i+1,j+1])
+                    for k in range(0, y.shape[1]):
+                        if y[i][k]==y[j][k] and y[i][k] == 1:
+                            ce = ce + 1
+                        if y[i][k]==y[j][k] and y[i][k] == 0:
+                            ce = ce + 1
+                        if y[i][k]!=y[j][k]:
+                            cd = cd + 1
+                    c_e.append(ce)  
+                    c_d.append(cd)
+                    print(ce)
+        print(c_e)
+        print(c_d)
+        print(indices)
+        
+        pass
+    
+    def generation(self):
+        #AND  y[i][j] = 1 iff y[i]=y[j]=1 else y[i][j]=0
+        #XOR  y[i][j] = 1 iff y[i]!=y[j]  else y[i][j]=0
+        #XNOR y[i][j] = 1 iff y[i]=y[j]   else y[i][j]=0
+        pass
 #def doSomething(self, a=None):
 #    if a is None:
 #        a = self.z
 #    self.z = 3
 #    self.b = a
-	
+    
